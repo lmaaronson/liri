763d9b0628c8a2b1dbd5fb813c4ecc9e1239d1eb
Laurie Aaronson <aaronson.laurie@gmail.com>
2018-04-05 18:19:04 -0500

    fixed do-what-it-says, hopefully

--- liri.js
+++ liri.js
@@ -8,7 +8,7 @@ var fs = require("fs")
 
 var Twitter = require('twitter');
 var Twitterclient = new Twitter(
-  keys.twitter                   //brings keys info into my Twitter object
+  keys.twitter //brings keys info into my Twitter object
 );
 
 
@@ -16,10 +16,10 @@ var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
 
-var firstChoice = process.argv[2]   //3rd item in the terminal input
+var firstChoice = process.argv[2] //3rd item in the terminal input
 console.log(firstChoice)
 
-var secondChoice = process.argv[3]   //4th item in the terminal input
+var secondChoice = process.argv[3] //4th item in the terminal input
 console.log(secondChoice)
 
 
@@ -29,113 +29,113 @@ inquirer.prompt([{
     name: "whichAPI",
     message: "Pick one",
     choices: ["my-twitter", "spotify-me", "movie-this", "do-what-it-says"]
-  }, 
-])
+  }, ])
 
-.then(function (user) {
+  .then(function (user) {
 
-    //either for loop to limit to 20 or check parameters to limit to 20
+      //either for loop to limit to 20 or check parameters to limit to 20
 
-    if (user.whichAPI === "my-twitter") {
-      var params = {
-        screen_name: 'lmaaronson'
-      };
-      
-      console.log(params)
-      Twitterclient.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
-          for (var i = 0; i < 4; i++) {
-            console.log(tweets[i].created_at);
-            console.log("");
-            console.log(tweets[i].text);
-        
-        }
-      }
-      
-  });
-} 
+      if (user.whichAPI === "my-twitter") {
+        var params = {
+          screen_name: 'lmaaronson'
+        };
 
+        console.log(params)
+        Twitterclient.get('statuses/user_timeline', params, function (error, tweets, response) {
+          if (!error) {
+            for (var i = 0; i < 4; i++) {
+              console.log(tweets[i].created_at);
+              console.log("");
+              console.log(tweets[i].text);
 
-    else if (user.whichAPI === "spotify-me") {
-      console.log("what song?");
-      inquirer.prompt([{
-            type: "input",
-            name: "song",
-            message: "write the name of a song",
-          },
-
-
-        ])
-
-        .then(function (user) {
-          spotify.search({
-            type: 'track',
-            query: user.song //finds the song the user inputs
-          }, function (err, data) {
-            if (err) {
-              return console.log('Error occurred: ' + err);
-            } else if (user.song) //this is the song name
-              // console.log(JSON.stringify(data, null, 4));  //data instead of body since that is what it is called in spotify
-              console.log("name of song: " + data.tracks.items[0].name);
-            console.log("name of artist: " + data.tracks.items[0].album.artists[0].name);
-            console.log("name of album: " + data.tracks.items[0].album.name);
-            console.log("Play the song: " + data.tracks.items[0].external_urls.spotify);
-          });
-
-        });
-
-
-    } else if (user.whichAPI === "movie-this") {
-      console.log("what movie?");
-      inquirer.prompt([{
-          type: "input",
-          name: "movie",
-          message: "which one",
-        }, ])
-
-        .then(function (user) {
-          var queryUrl = "http://www.omdbapi.com/?t=" + user.movie + "&y=&plot=short&apikey=trilogy";
-          console.log(queryUrl);
-          request(queryUrl, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-              var jsonData = JSON.parse(body)
-              console.log("Title: " + jsonData.Title);
-              console.log("Year: " + jsonData.Year);
-              console.log("Rated: " + jsonData.Rated);
-              console.log("IMDB Rating: " + jsonData.imdbRating);
-              console.log("Country: " + jsonData.Country);
-              console.log("Language: " + jsonData.Language);
-              console.log("Plot: " + jsonData.Plot);
-              console.log("Actors: " + jsonData.Actors);
-              console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
             }
-            console.log(error)
+          }
+
+        });
+      } else if (user.whichAPI === "spotify-me") {
+        console.log("what song?");
+        inquirer.prompt([{
+              type: "input",
+              name: "song",
+              message: "write the name of a song",
+            },
+
+
+          ])
+
+          .then(function (user) {
+            spotify.search({
+              type: 'track',
+              query: user.song //finds the song the user inputs
+            }, function (err, data) {
+              if (err) {
+                return console.log('Error occurred: ' + err);
+              } else if (user.song) //this is the song name
+                // console.log(JSON.stringify(data, null, 4));  //data instead of body since that is what it is called in spotify
+                console.log("name of song: " + data.tracks.items[0].name);
+              console.log("name of artist: " + data.tracks.items[0].album.artists[0].name);
+              console.log("name of album: " + data.tracks.items[0].album.name);
+              console.log("Play the song: " + data.tracks.items[0].external_urls.spotify);
+            });
+
           });
 
 
-          // var movieName = "minions"
-          // var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-          //  request(queryUrl, function (error, response, body) {
-          //  if (!error && response.statusCode === 200) { // If the request is successful
-          //  console.log("Release Year: " + JSON.parse(body).Year); // Parse the body of the site and recover just the imdbRating
-          //  console.log(response)
-          // }
-          // })
+      } else if (user.whichAPI === "movie-this") {
+        console.log("what movie?");
+        inquirer.prompt([{
+            type: "input",
+            name: "movie",
+            message: "which one",
+          }, ])
+
+          .then(function (user) {
+            var queryUrl = "http://www.omdbapi.com/?t=" + user.movie + "&y=&plot=short&apikey=trilogy";
+            console.log(queryUrl);
+            request(queryUrl, function (error, response, body) {
+              if (!error && response.statusCode === 200) {
+                var jsonData = JSON.parse(body)
+                console.log("Title: " + jsonData.Title);
+                console.log("Year: " + jsonData.Year);
+                console.log("Rated: " + jsonData.Rated);
+                console.log("IMDB Rating: " + jsonData.imdbRating);
+                console.log("Country: " + jsonData.Country);
+                console.log("Language: " + jsonData.Language);
+                console.log("Plot: " + jsonData.Plot);
+                console.log("Actors: " + jsonData.Actors);
+                console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+              }
+              console.log(error)
+            });
 
 
-        });
+            // var movieName = "minions"
+            // var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+            //  request(queryUrl, function (error, response, body) {
+            //  if (!error && response.statusCode === 200) { // If the request is successful
+            //  console.log("Release Year: " + JSON.parse(body).Year); // Parse the body of the site and recover just the imdbRating
+            //  console.log(response)
+            // }
+            // })
 
-    } else if (user.whichAPI === "do-what-it-says") {
-      console.log("i get to choose for you!");
-      
-      fs.readFile("random.txt", "utf8", function (error, data) {
-        if (error) {
-          return console.log(error);
-        }
-        console.log(data);
+
+          });
+
+      } else if (user.whichAPI === "do-what-it-says") {
+        console.log("i get to choose for you!");
+
         var dataArr = data.split(",");
-        console.log(dataArr);
 
-      });
-    }
-});
+
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (dataArr === 2) {
+              pick(dataArr[0], dataArr[1]);
+            } else if (dataArr === 2) {
+              pick(dataArr[0]);
+            }
+        }); console.log(data);
+
+          console.log(dataArr);
+
+        };
+    })
